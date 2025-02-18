3233a4cbdd6dfd6d87c95598e2f4e4382458819f
UKS <skulptorrr@duck.com>
2025-02-18 16:55:46 +0200

    NodeMapView.swift

--- MindMap_test/View Stack/NodeMapView.swift
+++ MindMap_test/View Stack/NodeMapView.swift
@@ -5,39 +5,62 @@ import Essentials
 struct NodeMapView: View {
     @ObservedObject var selection: SelectionHandler
     @Binding var nodes: [Node]
-    @State var inEdit: Bool = false
+    @State var inEdit: UUID?
+    
+    @State var text = ""
     
     var body: some View {
         ZStack {
             Color.clickableAlpha
                 .onTapGesture {
                     self.selection.selectClear()
+                    if self.inEdit != nil {
+                        self.inEdit = nil
+                    }
                 }
             
             ForEach(nodes, id: \.visualID) { node in
                 NodeView(node: node, selection: self.selection)
+                    .backgroundStyle(Color.clickableAlpha)
+                    .overlay { EditView(node: node) }
                     .offset(x: node.position.x, y: node.position.y)
-                    .onTapGesture {
-                        if NSApp.currentEvent!.modifierFlags.check(oneOf: [.shift, .command]) {
-                            self.selection.selectNodeSwitch(node)
-                        } else {
-                            self.selection.selectNode(node)
+                    .gesture(
+                        TapGesture(count: 2).onEnded {
+                            text = node.text
+                            inEdit = node.id
+                        })
+                        .simultaneousGesture(TapGesture().onEnded {
+                            if self.inEdit != nil {
+                                self.inEdit = nil
+                            }
+                            
+                            if NSApp.currentEvent!.modifierFlags.check(oneOf: [.shift, .command]) {
+                                self.selection.selectNodeSwitch(node)
+                            } else {
+                                self.selection.selectNode(node)
+                            }
                         }
-                    }
-                    .overlay {
-//                        if inEdit {
-//                            
-//                            TextField("Type the text of new mind…", text: $model.selection.editingText, onCommit: {
-//                                if let node = model.selection.onlySelectedNode(in: model.mesh) {
-//                                    model.mesh.updateNodeText(node, string: self.model.selection.editingText)
-//                                }
-//                            })
-//                            .textFieldStyle(.roundedBorder)
-//                        }
-                    }
+                    )
             }
         }
     }
+    
+    @ViewBuilder
+    func EditView(node: Node) -> some View {
+        if inEdit == node.id {
+            if let id = nodes.firstIndex(where: { $0.id == node.id }) {
+                TextField(nodes[id].text, text: $text, onCommit: {
+                    nodes[id].text = text
+                    inEdit = nil
+                })
+                .textFieldStyle(.roundedBorder)
+                .frame(minWidth: 300)
+                .padding(5)
+                .background(Color.black)
+            }
+        }
+    }
+    
 }
 
 /////////////////////
@@ -52,5 +75,12 @@ struct NodeMapView_Previews: PreviewProvider {
     static var previews: some View {
         let selection = SelectionHandler()
         return NodeMapView(selection: selection, nodes: $nodes)
+    }
+}
+
+extension NSTextField {
+    open override var focusRingType: NSFocusRingType {
+            get { .none }
+            set { }
     }
 }
